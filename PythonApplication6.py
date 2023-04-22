3a4f9feefbda78868e261e67a988f1796356270d
Ahmed Einshouka <76920911+ahmedeinshouka@users.noreply.github.com>
2023-04-23 01:14:06 +0200

    other one I use lists in it to make it more easy

--- PythonApplication6.py
+++ PythonApplication6.py
@@ -1,4 +1,29 @@
 ﻿import random 
+rocket="""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+papper="""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""
+scissors="""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+gaming_images=[rocket,papper,scissors]
 print("**************************************************************")
 print("ROCK PAPER SCISSORS Game")
 print("**************************************************************")
@@ -6,64 +31,22 @@ choice=int(input("What do you choose? Type 0 for Rock, 1 for paper or 2 for scis
 print("**************************************************************")
 print("your choice:")
 if choice==0:
-    print("""
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-""")
+    print(gaming_images[0])
 elif choice==1:
-     print("""
-     _______
----'    ____)____
-           ______)
-          _______)
-         _______)
----.__________)
-""")
+     print(gaming_images[1])
 elif choice ==2:
-    print("""
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-""")
+    print(gaming_images[2])
 else:
     print(" please check your choice again !!!")
 computer_choice=random.randint(0,2)
 print("*******************************************************************")
 print("your computer choice:")
 if computer_choice==0:
-    print("""
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-""")
+    print(gaming_images[0])
 elif computer_choice==1:
-     print("""
-     _______
----'    ____)____
-           ______)
-          _______)
-         _______)
----.__________)
-""")
+      print(gaming_images[1])
 elif computer_choice ==2:
-    print("""
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-""")
+   print(gaming_images[2])
 print("*******************************************************************************************************")
 if computer_choice==0 and choice==2:
     print("You lose")
@@ -86,6 +69,4 @@ elif computer_choice==2 and choice==0:
 elif computer_choice==2 and choice==1:
     print("You lose")
 print("**************************************************************************************************************")
-
-
 
